56da1824ecd3fe5e60485ceb8358c871be4a5ad3
Vasai Robert <vasairobert@yahoo.com>
2026-03-12 16:52:09 +0200

    Added background texture

--- src/Entry/Card.jsx
+++ src/Entry/Card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useCallback, useEffect, useRef } from "react";
 import { useUpdateEntry } from "./entryQueryHooks";
 import { useEntryContext } from "./context";
